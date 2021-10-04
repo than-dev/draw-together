1b3e70353ddc25c7b8c45f08959054f4ebb2eab0
NathanCotrim <nathan.cotrim@gmail.com>
2021-10-04 11:15:59 -0300

    refactor: ws communication is okay

--- client/socket.js
+++ client/socket.js
@@ -3,25 +3,21 @@ let ws;
 (async function () {
     ws = await connectToServer()
     
-    ws.emit = (event, data) => {
-        ws.send(JSON.stringify({
-            event,
-            data
-        }))
-    }
-        
+    ws.send(JSON.stringify({
+        event: 'teste',
+        data: 'It\'s a test'
+    }))
+    
     ws.onmessage = (webSocketMessage) => {
         const messageBody = JSON.parse(webSocketMessage.data);
         
         console.log('Data received successfully', messageBody);
     };
-
-   ws.emit('teste', 'Ola, its a test')
 })();
 
 async function connectToServer() {
     const ws = new WebSocket('ws://localhost:7071/ws');
     return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(ws), 100)
+        setInterval(() => resolve(ws), 100)
     });
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,5 @@
+let currentWS;
+
 const socketFunctions = require('./socket-functions')
 
 const WebSocket = require('ws');
@@ -6,17 +8,26 @@ const wss = new WebSocket.Server({ port: 7071 });
 const clients = new Map()
 
 wss.on('connection', (ws) => {
+    currentWS = ws
     const id = uuidv4();
     const color = Math.floor(Math.random() * 360);
     const metadata = { id, color };
 
     clients.set(ws, metadata);
   
-    console.log(clients.get(ws));
-
-    ws.on('message', async (messageAsString) => {
+  console.log(clients.get(ws));
+  
+    ws.addListener('send', (message) => {
+      ws.send(JSON.stringify({
+        message
+      }))  
+    })
+    console.log(ws.eventNames())
+    
+    
+    ws.addListener('message', async (messageAsString) => {
       const message = await JSON.parse(messageAsString);
-      
+      console.log(message);
       const messageEvent = message.event
 
       await socketFunctions[messageEvent]()
@@ -25,16 +36,13 @@ wss.on('connection', (ws) => {
 
       message.sender = metadata.id;
       message.color = metadata.color
-
-       ws.send(JSON.stringify({
-        message: 'You communicate with server successfully'
-      }))
     })
-  
+    
     ws.on('close', () => {
         clients.delete(ws);
     });
 })
+
 
 function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -42,3 +50,5 @@ function uuidv4() {
     return v.toString(16);
   });
 }
+
+setTimeout(() => currentWS.emit('send', 'TESTANDOOOOOO'), 5000)
