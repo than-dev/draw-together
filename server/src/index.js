06a2ea41531195128182fcaeb6d2da5218a33ac7
NathanCotrim <nathan.cotrim@gmail.com>
2021-10-05 00:56:44 -0300

    feat: finish server

--- server/src/index.js
+++ server/src/index.js
@@ -1,56 +1,32 @@
-let currentWS;
-
-const socketFunctions = require('./socket-functions')
-
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 7071 });
+const uuidv4 = require('./uuidv4')
 
 const clients = new Map()
 
 wss.on('connection', (ws) => {
-    currentWS = ws
-    const id = uuidv4();
-    const color = Math.floor(Math.random() * 360);
-    const metadata = { id, color };
-
-    clients.set(ws, metadata);
-    console.log(clients.get(ws));
+  const id = uuidv4();
   
-    ws.addListener('send', (message) => {
-      ws.send(JSON.stringify({
-        event: 'start',
-        message
-      }))  
+  clients.set(ws, id);
+
+  ws.addListener('message', async (messageAsString) => {
+    const message = await JSON.parse(messageAsString);
+
+    [...clients.keys()].forEach(client => {
+      const clientId = clients.get(client)
+
+      if (clientId !== id) {
+        client.send(JSON.stringify({
+          event: message?.event,
+          data: {
+            ...message?.data
+          }
+        }))
+      }
     })
-    
-    ws.addListener('message', async (messageAsString) => {
-      const message = await JSON.parse(messageAsString);
-      console.log(message);
-      const messageEvent = message.event
+  })
 
-      await socketFunctions[messageEvent]()
-
-      const metadata = clients.get(ws);
-
-      message.sender = metadata.id;
-      message.color = metadata.color
-    })
-  
-    ws.addEventListener('draw', (data) => {
-      console.log(data);
-    })
-    
-    ws.on('close', () => {
-        clients.delete(ws);
-    });
-  
-    setTimeout(() => ws.emit('send', 'TESTANDOOOOOO'), 5000)
-})
-
-
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
+  ws.on('close', () => {
+      clients.delete(ws);
   });
-}
+})
